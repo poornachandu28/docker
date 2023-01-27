77e880c6c76c8a1b17b9771ad61878ddc7e23d9b
poornachandu28 <110621498+poornachandu28@users.noreply.github.com>
2023-01-27 16:27:14 +0530

    Update terraform1.tf

--- terraform1.tf
+++ terraform1.tf
@@ -1,11 +1,11 @@
 provider "aws" {
     region = "us-west-1"
-    access_key = "AKIATPY2YNP4YXLPATKJ"
-    secret_key = "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"
+    access_key = "AKIA2NCQBFS5K2YJB7U7"
+    secret_key = "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"
 }
 
 resource "aws_instance" "ubuntu" {
-    ami = "ami-00d8a762cb0c50254"
+    ami = "ami-06ee4e2261a4dc5c3"
     instance_type = "t2.micro"
     subnet_id="subnet-070c28363782d3866"
     security_groups=["sg-00ec4cd9debac6427"]
